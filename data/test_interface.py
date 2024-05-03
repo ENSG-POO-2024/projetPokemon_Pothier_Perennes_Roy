54bcd20d9586d654866135e9bdded27aa7fa70bf
colineroy <coline.roy13@gmail.com>
2024-05-03 16:40:02 +0200

    voivi les codes pour la première interface

---
+++ data/test_interface.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Éditeur de Spyder
+
+Ceci est un script temporaire.
+"""
+
+import sys
+from interface_entre import Ui_Dialog
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+
+class XXXXWindow (QMainWindow, Ui_Dialog):
+    def __init__(self, parent=None):
+        super(XXXXWindow, self).__init__(parent)
+        self.setupUi(self)
+# ...
+if __name__ == "__main__":
+    def run_app():
+        app = QApplication(sys.argv)
+        mainWin = XXXXWindow()
+        mainWin.show()
+        app.exec_()
+    run_app()
